cff3c782ef4667064b62e529200d6bbbbb62fb6d
Himanshu Chaurasia <106426822+Himanshuchaurasia9@users.noreply.github.com>
2023-06-02 19:22:41 +0530

    update

--- Himanshu/urls.py
+++ Himanshu/urls.py
@@ -4,10 +4,14 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 
+
+
+
+
 urlpatterns = [
     path('adminp/', admin.site.urls),
     path('',include("App.urls")),
     path('admin/',include("Backend.urls")),
-]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-
-
+]
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
