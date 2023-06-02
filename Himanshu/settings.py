c9e6a99ff91bfde3952cc4dfa712559ba57b1644
Himanshu Chaurasia <106426822+Himanshuchaurasia9@users.noreply.github.com>
2023-06-02 19:05:36 +0530

    Update settings.py

--- Himanshu/settings.py
+++ Himanshu/settings.py
@@ -120,13 +120,14 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = "/var/www/example.com/static/"
 STATICFILES_DIRS = [
-     os.path.join(BASE_DIR / "static"),
+    BASE_DIR / "static",
+    "/var/www/static/",
 ]
 
 MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR / "media")
+MEDIA_ROOT = "/var/www/example.com/media/"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
