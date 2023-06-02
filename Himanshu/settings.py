3fa73cf4cbabaac321710c3c232958a37537fecb
Himanshu Chaurasia <106426822+Himanshuchaurasia9@users.noreply.github.com>
2023-06-02 18:52:46 +0530

    Update settings.py

--- Himanshu/settings.py
+++ Himanshu/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-fgu+=vfgk01v#gt1z+@%ryje7dl^wcqgn)(z*61&-&^26j2iz5'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ["*"]
 
@@ -126,7 +126,7 @@ STATICFILES_DIRS = [
 ]
 
 MEDIA_URL = '/media/'
-MEDIA_ROOT = BASE_DIR / "media"
+MEDIA_ROOT = os.path.join(BASE_DIR / "media")
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
